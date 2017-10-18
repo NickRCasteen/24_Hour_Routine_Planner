2fb1fdc7e289a6c04db50e6ed11135c82456f2ac
Sieve <sieve32@gmail.com>
2017-10-18 16:08:06 -0500

    Initial
    Handles data directory, creates if needed and doesn't fail if already
    exists

--- DataStructure.py
+++ DataStructure.py
@@ -1,7 +1,17 @@
 #Data Structures
 #TO-DO
+import os
+import glob
 
 class datastruct:
 
-	def __init__(self):
-		pass
+    def __init__(self):
+        p = 'Data/'
+
+        try:
+            os.makedirs(p)
+        except OSError:
+            if not os.path.isdir(p):
+                raise
+
+d = datastruct()
