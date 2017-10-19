987dc08d07c767b5c0f9d4c11f0083ad288f7bda
Sieve <sieve32@gmail.com>
2017-10-18 19:47:18 -0500

    Import/Export Prep
    Prepwork for file import and export

--- DataStructure.py
+++ DataStructure.py
@@ -4,14 +4,41 @@ import os
 import glob
 
 class datastruct:
+    dataPath = 'Data/'
 
     def __init__(self):
-        p = 'Data/'
-
         try:
-            os.makedirs(p)
+            os.makedirs(self.dataPath)
         except OSError:
-            if not os.path.isdir(p):
+            if not os.path.isdir(self.dataPath):
                 raise
+
+        self.print_files()
+
+    def print_files(self):
+        files = glob.glob(os.path.join(self.dataPath, '*.sch'))
+        i = 1
+        for infile in files:
+            f = open(infile)
+            print "[{}] : {}".format(i, f.read())
+            f.close()
+            i += 1
+
+    def import_file(self, fpath):
+        f = open(fpath)
+        ##Do_stuff
+        f.close()
+
+    def export_file(self, flag):
+        if(flag == "Schedule"):
+            fpath = 'out.sch'
+        elif(flag == "Task"):
+            fpath = 'out.tsk'
+        f = open(fpath, "w")
+        f.writelines("Output Name")
+        ##Do_stuff
+        f.close()
+
+    
 
 d = datastruct()
