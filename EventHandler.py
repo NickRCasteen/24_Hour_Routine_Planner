1cc1ab74eaf4a0a3c69395474deb7dd64b0340a8
Nicholas Reno Casteen <NickRCasteen@gmail.com>
2017-10-19 22:43:48 -0500

    User Story Update 02 with UnitTest

--- EventHandler.py
+++ EventHandler.py
@@ -7,6 +7,8 @@ class eventhandler:
 	def __init__(self):
 		pass
 
-	def Add(self, hr1, mt1, half1, notify_level):
+	def Add(self, hr1, mt1, hr2, mt2, half1, half2, notify_level):
 		print "Run Add in Event Handler!" #Replace this with code that actually changes this.
+		if hr2 != 0:
+			print "Range Event time!"
 		return;
---
+++ tests/Add_Event_Test_sans_Framework.py
@@ -0,0 +1,44 @@
+from  UI import *
+
+#This will test for being able to add user stories to the schedule.
+#The demands will be made in the same order the User is expected to make them. 
+
+Run_Program = ui()
+
+Run_Program.hour1 = 11;
+Run_Program.minute1 = 3;
+Run_Program.timehalf1 = "PM";
+Run_Program.notification = 1;
+Run_Program.AddEvent() #Adds an event at 11:03 PM with notification level 1
+#Notice we're hard setting the values right here. Much like how the UI will. The user will put in their values and they'll be passed along.
+
+Run_Program.hour1 = 12;
+Run_Program.minute1 = 16;
+Run_Program.timehalf1 = "AM";
+Run_Program.notification = 3;
+Run_Program.AddEvent() #And another for 12:16 AM with notification level 3.
+
+Run_Program.hour1 = 32;
+Run_Program.minute1 = 22;
+Run_Program.timehalf1 = "AM";
+Run_Program.notification = 7;
+Run_Program.AddEvent() #This should throw an error that hour cannot be >12 and end the attempt to add an event
+
+Run_Program.hour1 = 9;
+Run_Program.minute1 = 66;
+Run_Program.timehalf1 = "PM";
+Run_Program.notification = 2;
+Run_Program.AddEvent() #This should throw an error that minute cannot be >60
+
+Run_Program.hour1 = 6;
+Run_Program.minute1 = 12;
+Run_Program.timehalf1 = "sadsafasf";
+Run_Program.notification = 1;
+Run_Program.AddEvent() #This should throw an Error that user must specify AM or PM
+
+Run_Program.userinput = "3:54"
+Run_Program.timehalf1 = "AM"
+Run_Program.ParseTime() #This runs a test for parsing the string into sets of numbers.
+
+Run_Program.userinput = "2:47 AM"
+Run_Program.ParseTime() #This should throw an error, stating that AM/PM is not asked for yet.
